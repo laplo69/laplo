14bb091e64c8bb8357eff378f085c0b7ed6054e1
quoctoank4it@gmail.com <quoctoank4it@gmail.com>
2024-06-30 11:26:58 +0700

    edit file

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ function App() {
     const shuffled = [...t].sort(() => Math.random() - 0.5);
     setRandomizedData(shuffled);
 
-    // Vô hiệu hóa chuột phải và tổ hợp phím xem mã nguồn
-    document.addEventListener("contextmenu", (event) => event.preventDefault());
-    document.onkeydown = function (e) {
-      if (
-        e.keyCode === 123 || // F12
-        (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) || // Ctrl+Shift+I
-        (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) || // Ctrl+Shift+J
-        (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) // Ctrl+U
-      ) {
-        return false;
-      }
-    };
+    // // Vô hiệu hóa chuột phải và tổ hợp phím xem mã nguồn
+    // document.addEventListener("contextmenu", (event) => event.preventDefault());
+    // document.onkeydown = function (e) {
+    //   if (
+    //     e.keyCode === 123 || // F12
+    //     (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) || // Ctrl+Shift+I
+    //     (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) || // Ctrl+Shift+J
+    //     (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) // Ctrl+U
+    //   ) {
+    //     return false;
+    //   }
+    // };
 
     return () => {
       // Dọn dẹp khi component bị unmount
@@ -87,29 +87,39 @@ function App() {
         width: "100%",
       }}
     >
-      <Menu
-        onRandom={handleRandom}
-        onNewest={handleNewest}
-        onOldest={handleOldest}
-        onFavorite={handleFavorite}
-      />
-      {randomizedData.map(
-        (image) =>
-          image.src !== "" && (
-            <img
-              key={image.id}
-              src={image.src}
-              alt="image.id"
-              style={{ width: "100%", maxWidth: 500 }}
-            />
-          )
-      )}
-      {isVisible && (
-        <button className="scroll-to-top" onClick={scrollToTop}>
-          {/* <BiSolidArrowToTop size={30} /> */}
-          <img src={icon} style={{ width: 40, height: 40 }}></img>
-        </button>
-      )}
+      <div
+        style={{
+          display: "grid",
+          justifyItems: "center",
+          gap: 10,
+          width: "100%",
+          maxWidth: 500,
+        }}
+      >
+        <Menu
+          onRandom={handleRandom}
+          onNewest={handleNewest}
+          onOldest={handleOldest}
+          onFavorite={handleFavorite}
+        />
+        {randomizedData.map(
+          (image) =>
+            image.src !== "" && (
+              <img
+                key={image.id}
+                src={image.src}
+                alt="image.id"
+                style={{ width: "100%", maxWidth: 500 }}
+              />
+            )
+        )}
+        {isVisible && (
+          <button className="scroll-to-top" onClick={scrollToTop}>
+            {/* <BiSolidArrowToTop size={30} /> */}
+            <img src={icon} style={{ width: 40, height: 40 }}></img>
+          </button>
+        )}
+      </div>
     </div>
   );
 }
