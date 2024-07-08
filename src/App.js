e917ca1eb4bbd5fa1aa42b1b95032cc93efa723d
quoctoank4it@gmail.com <quoctoank4it@gmail.com>
2024-07-08 22:11:55 +0700

    edit file

--- src/App.js
+++ src/App.js
@@ -9,18 +9,50 @@ function App() {
   const [randomizedData, setRandomizedData] = useState([]);
 
   useEffect(() => {
-    const images = {};
-    function importAll(r) {
-      r.keys().forEach((key) => (images[key] = r(key)));
-    }
+    // const images = {};
+    // function importAll(r) {
+    //   r.keys().forEach((key) => (images[key] = r(key)));
+    // }
 
-    importAll(require.context("./images", false, /\.(png|jpe?g|svg)$/));
+    // importAll(require.context("./images", false, /\.(png|jpe?g|svg)$/));
 
-    const imageArray = Object.keys(images).map((key) => ({
-      id: parseInt(key.replace("./", "").replace(".jpg", "")),
-      src: images[key].default || images[key],
+    // const imageArray = Object.keys(images).map((key) => ({
+    //   id: parseInt(key.replace("./", "").replace(".jpg", "")),
+    //   src: images[key].default || images[key],
+    // }));
+    // console.log(imageArray);
+    // setImages(imageArray);
+    // const shuffled = [...imageArray].sort(() => Math.random() - 0.5);
+    // setRandomizedData(shuffled);
+
+    const imageFiles = [
+      "1.jpg",
+      "2.jpg",
+      "3.jpg",
+      "4.jpg",
+      "5.jpg",
+      "6.jpg",
+      "7.jpg",
+      "8.jpg",
+      "9.jpg",
+      "10.jpg",
+      "11.jpg",
+      "12.jpg",
+      "13.jpg",
+      "14.jpg",
+      "15.jpg",
+      "16.jpg",
+      "17.jpg",
+      "18.jpg",
+      "19.jpg",
+      "20.jpg",
+    ];
+
+    const imageArray = imageFiles.map((fileName) => ({
+      id: parseInt(imageFiles.replace(".jpg", "")),
+      src: `https://laplo69.com/images/${fileName}`,
     }));
-    console.log(imageArray);
+
     setImages(imageArray);
     const shuffled = [...imageArray].sort(() => Math.random() - 0.5);
     setRandomizedData(shuffled);
